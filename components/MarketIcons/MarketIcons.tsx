714cac700d2753bb09617ee8585ccbdb42bd1af7
Nathan Drake <nathandrakedrums@gmail.com>
2023-01-28 00:01:14 -0600

    fix: Token id zero market links

--- components/MarketIcons/MarketIcons.tsx
+++ components/MarketIcons/MarketIcons.tsx
@@ -1,7 +1,12 @@
 import Image from 'next/image';
 import { Tooltip } from 'react-tooltip';
 import { type Project } from 'components/LandingPage/Projects/projects';
-import { icons, getProjectMarketLink, Market, getTokenMarketLink } from './icons';
+import {
+  icons,
+  getProjectMarketLink,
+  Market,
+  getTokenMarketLink,
+} from './icons';
 import * as St from './MarketIcons.styled';
 
 interface Props {
@@ -14,29 +19,44 @@ const MarketIcons = ({ project, tokenId }: Props): JSX.Element => {
 
   const iconSize = 20;
 
+  // need this since zero is falsy
+  const isToken = tokenId !== undefined;
+
   return (
     <St.Container>
       {icons.map((icon) => {
         // skip etherscan for token version
-        if (icon.market === Market.Etherscan && tokenId) return null;
-        const { src, id, altCollection, altToken, tooltipCollection, tooltipToken } =
-          icon;
+        if (icon.market === Market.Etherscan && isToken) return null;
+        const {
+          src,
+          id,
+          altCollection,
+          altToken,
+          tooltipCollection,
+          tooltipToken,
+        } = icon;
 
-        const alt = tokenId ? altToken : altCollection;
-        const tooltip = tokenId ? tooltipToken : tooltipCollection;
-        const link = tokenId
+        const alt = isToken ? altToken : altCollection;
+        const tooltip = isToken ? tooltipToken : tooltipCollection;
+        const link = isToken
           ? getTokenMarketLink(icon, contractAddress, tokenId)
           : getProjectMarketLink(icon, openSeaSlug, contractAddress);
 
         return (
           <a key={id} href={link} target="_blank" rel="noreferrer">
-            <Image id={id} src={src} alt={alt} width={iconSize} height={iconSize} />
+            <Image
+              id={id}
+              src={src}
+              alt={alt}
+              width={iconSize}
+              height={iconSize}
+            />
 
             <Tooltip
               anchorId={id}
               content={tooltip}
               positionStrategy="fixed"
-              place={tokenId ? 'top' : 'bottom'}
+              place={isToken ? 'top' : 'bottom'}
             />
           </a>
         );
