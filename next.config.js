85c60d82fb26b162b2ad9e190708104897035f58
Nathan Drake <nathandrakedrums@gmail.com>
2023-10-07 18:16:12 -0500

    chore: Back to 13.3

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  experimental: {
+    appDir: false,
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
