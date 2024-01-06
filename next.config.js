333486a1b650eb380b66b7c5bc517ce8960bcb4b
Nathan Drake <nathandrakedrums@gmail.com>
2024-01-06 13:59:18 -0600

    chore: More sort configs

--- next.config.js
+++ next.config.js
@@ -1,54 +1,54 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  output: "standalone",
-  reactStrictMode: true,
-  swcMinify: true,
   compiler: {
     styledComponents: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        hostname: "mattoapi.blob.core.windows.net",
+        pathname: "/**",
+        protocol: "https",
+      },
+      {
+        hostname: "127.0.0.1",
+        pathname: "/**",
+        protocol: "http",
+      },
+      {
+        hostname: "arweave.net",
+        pathname: "/dtEayxAD2Aknd8g8WWyErEX37kesMRsJhbopwDYPhdo/**",
+        protocol: "https",
+      },
+    ],
+    unoptimized: true,
+  },
+  output: "standalone",
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        destination: "/",
+        permanent: true,
+        source: "/project",
+      },
+      {
+        destination: "/project/:slug",
+        permanent: true,
+        source: "/project/:slug/token",
+      },
+    ];
+  },
+  swcMinify: true,
   webpack(config) {
     config.module.rules.push({
-      test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
+      test: /\.svg$/i,
       use: ["@svgr/webpack"],
     });
     // Web3Modal
     config.externals.push("pino-pretty", "lokijs", "encoding");
 
     return config;
-  },
-  images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "mattoapi.blob.core.windows.net",
-        pathname: "/**",
-      },
-      {
-        protocol: "http",
-        hostname: "127.0.0.1",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "arweave.net",
-        pathname: "/dtEayxAD2Aknd8g8WWyErEX37kesMRsJhbopwDYPhdo/**",
-      },
-    ],
-  },
-  async redirects() {
-    return [
-      {
-        source: "/project",
-        destination: "/",
-        permanent: true,
-      },
-      {
-        source: "/project/:slug/token",
-        destination: "/project/:slug",
-        permanent: true,
-      },
-    ];
   },
 };
