527ce15c2240c8c468f9a15e28cd1ff31903281f
Nathan Drake <nathandrakedrums@gmail.com>
2024-02-05 21:00:06 -0600

    test: Swap to vitest

--- utils/types.ts
+++ utils/types.ts
@@ -8,6 +8,7 @@ import { isString } from "./helpers";
 export type SetState<T> = React.Dispatch<React.SetStateAction<T>>;
 
 export const addressRegex = /^0x[a-fA-F0-9]{40}$/;
+
 export const AddressZod = z.custom<Address>((value) => {
   if (!isString(value) || !addressRegex.test(value)) {
     throw new Error("Invalid address");
